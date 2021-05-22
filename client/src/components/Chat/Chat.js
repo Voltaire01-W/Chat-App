b30ad962e01188b831147c80b87bc1ad05a75792
Voltaire01-W <59457919+Voltaire01-W@users.noreply.github.com>
2021-05-22 10:16:24 -0500

    Added Infobar and Input components with stylings for the Chatrooms

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+import Messages from '../Messages/Messages';
 
 import './Chat.css';
 
@@ -45,12 +48,9 @@ const Chat = ({ location }) => {
         
         <div className="outerContainer">
             <div className="container">
-                <InfoBar />
-                {/* <input 
-                    value={message} 
-                    onChange={(event) => setMessage(event.target.value)}
-                    onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
-                /> */}
+                <InfoBar room={room}/>
+                <Messages />
+                <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
 
             </div>
         </div>
